5786da349b1022fa7448a69cd37df92f78483fa0
Vincenzo Maisto <maisto_v@libero.it>
2023-05-26 17:21:14 +0000

    edit scirpt scp_to_slaves.sh

--- scp_to_slaves.sh
+++ scp_to_slaves.sh
@@ -1,8 +1,8 @@
-scp .libs/libisal.so slave1:/home/a3cube/libisal-OPAE.so
-scp .libs/libisal.so slave2:/home/a3cube/libisal-OPAE.so
-scp .libs/libisal.so slave3:/home/a3cube/libisal-OPAE.so
-scp .libs/libisal.so slave4:/home/a3cube/libisal-OPAE.so
+scp .libs/libisal.so slave1:~/libisal-OPAE.so
+scp .libs/libisal.so slave2:~/libisal-OPAE.so
+scp .libs/libisal.so slave3:~/libisal-OPAE.so
+scp .libs/libisal.so slave4:~/libisal-OPAE.so
 
 echo "Remember to install on each slave now, run:"
-echo "	sudo cp libisal-OPAE.so /usr/lib/libisal-OPAE.so"
+echo "	sudo cp ~//libisal-OPAE.so /usr/lib/libisal-OPAE.so"
 
